1c6c47df2151535e8a043f8a4d7a2aff5d8b83d3
hathout <khkhaledhathout@gmail.com>
2019-01-25 03:39:37 +0300

    Date 25/01/2019 01

--- models/__init__.py
+++ models/__init__.py
@@ -15,6 +15,7 @@
 #    GNU Affero General Public License for more details.
 #
 #    You should have received a copy of the GNU Affero General Public License
+# Khaled Hathout
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #
--- models/fleet.py
+++ models/fleet.py
@@ -16,7 +16,7 @@
 #
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+# Khaled Hathout
 #
 
 from openerp import api, fields, models
@@ -38,5 +38,5 @@ class FleetVehicle(models.Model):
         return res
 
     # Fields
-    #
+    # Many to one
     department = fields.Many2one('hr.department', default=_get_user_department)
